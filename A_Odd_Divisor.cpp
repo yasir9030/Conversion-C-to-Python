283e024b1d9bad44851ef491065d5ae383e0d74d
yasir9030 <yasir.ice.7@gmail.com>
2025-11-20 00:22:59 +0600

    Add files via upload

---
+++ A_Odd_Divisor.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int t;
+/*
+cin >> t;
+
+for(int i = 0; i < t; i++)
+{
+  long long int n;
+  cin >> n;
+
+  if(n % 2 != 0)
+  cout <<"YES" << endl;
+  else
+  {
+
+  }
+
+}   */
+
+cout << sqrt(2);
+
+
+
+return 0;    
+}
