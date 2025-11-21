981ca21ee25b1fab139f083b4049089d4078022b
Yasir Arafat <yasir.ice.7@gmail.com>
2025-11-21 11:17:19 +0600

    Update format_string.py

--- format_string.py
+++ format_string.py
@@ -1,4 +1,5 @@
 #format string 
+#To specify a string as an f-string, simply put an f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations.
 age=23
 txt=f"my name is Yasir ,my {age}"
 print(txt)
