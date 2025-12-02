eab9581c8c027a4d53965e101096326530c83007
Yasir Arafat <yasir.ice.7@gmail.com>
2025-12-02 09:37:56 +0600

    Create matching.py

---
+++ matching.py
@@ -0,0 +1,19 @@
+day=4
+match day :
+      case 1:
+            print("monday")
+      case 2:
+            print("tuesday")
+      case 3:
+            print("wedensday")
+      case 4:
+            print("Thrusday")
+      case 5:
+            print ("friday")
+      case 6:
+            print("saturday")
+      case 7:
+            print("sunday") 
+            
+
+
